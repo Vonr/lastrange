27d0014dd302b877d2663de3b82d76b5aebc9df6
Vonr <dtaclanlegion@gmail.com>
2022-10-07 22:29:10 +0800

    Use rug to increase precision

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 ## A Lagrange Interpolator for string
 
-Lastrange outputs valid Rust code which given x, the position of the character,
-gives back the character in the xth position of the originally encoded string
+Lastrange outputs a mathematical formula which given n, the position of the character,
+gives back the character in the nth position of the originally encoded string
 up to an untested precision.
 
 This could perhaps be used in some strange obfuscation technique but was created for fun.
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use std::env;
+use std::{cmp::Ordering, env, i128};
+
+use rug::{Complete, Integer, Rational};
 
 fn main() {
     let mut s = String::new();
@@ -9,47 +11,42 @@ fn main() {
 }
 
 fn lagrange_string_interp(s: &str) -> String {
-    let xs: Vec<f64> = (0..s.len()).map(|i| i as f64).collect();
-    let ys = s.chars().map(|c| c as u8 as f64).collect::<Vec<f64>>();
+    let one: Rational = Rational::from(1);
+    let xs: Vec<i128> = (0..s.len()).map(|i| i as i128).collect();
+    let ys: Vec<i128> = s.chars().map(|c| c as i128).collect();
 
-    let mut result = String::from("(");
+    let mut result = String::with_capacity(256);
 
-    for (i, x) in xs.iter().enumerate() {
-        let mut y = ys[i];
-        let mut numerator = String::new();
-        let mut denominator = 1f64;
-
+    let mut numerator = String::new();
+    for ((i, x), y) in xs.iter().enumerate().zip(ys) {
         if i > 0 {
-            result.push_str("+");
+            result.push('+');
         }
 
-        for (j, xj) in xs.iter().enumerate() {
-            if i == j {
-                continue;
-            }
-
-            if xj == &0.0 {
-                numerator += &format!("x*");
-            } else {
-                numerator += &format!("(x-{:.}.0)*", xj);
-            }
-            denominator *= x - xj;
-        }
-        y /= denominator;
+        let mut denominator = Integer::from(1);
+        xs.iter()
+            .enumerate()
+            .filter(|(j, _)| i != *j)
+            .for_each(|(_, xj)| {
+                match *xj == 0 {
+                    true => numerator += "x*",
+                    false => numerator += &format!("(x-{:.}.0)*", xj),
+                };
+                denominator *= x - xj
+            });
+        let y = Rational::from(y) / denominator;
 
         numerator.pop();
 
-        if y == 1.0 {
-            result += &numerator;
-        } else {
-            if y.fract() == 0.0 {
-                result += &format!("{:.}.0*{:.}", y, numerator);
-            } else {
-                result += &format!("{:.}*{:.}", y, numerator);
+        match y == one {
+            true => result += &numerator,
+            false if y.rem_trunc_ref().complete().cmp0() == Ordering::Equal => {
+                result += &format!("{:.}.0*{:.}", y.to_f64(), numerator)
             }
+            _ => result += &format!("{:.}*{:.}", y.to_f64(), numerator),
         }
+        numerator.clear()
     }
 
-    result.push_str(").round() as u8 as char");
     result.replace("+-", "-")
 }
